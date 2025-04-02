6f2a4cec3cb3d9f1361bd092d0af8a81704f885e
unknown <manimani93444@gmail.com>
2025-04-02 15:51:12 +0530

    upload files 3

--- test1.py
+++ test1.py
@@ -2,4 +2,4 @@ import sys
 print ("jenkins Build using parameters ! ")
 X_VALUE = int (sys.argv[1])
 Y_VALUE = int (sys.argv[2])
-print ("Mutiply =",(X_VALUE * Y_VALUE))
+print ("Multiply =",(X_VALUE * Y_VALUE))
